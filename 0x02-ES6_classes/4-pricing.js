22f5d3b8e65471d250cf370e274a2bc042bb19e3
MahiET <meazalemma2020@gmail.com>
2022-06-17 08:36:48 +0000

    Currency

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,35 +1,31 @@
 import Currency from './3-currency';
 
-export default class pricing {
-    constructor(amount, currency){
-	this.amount = amount;
-	this.currency = currency;
-    }
-
-    get amount(){
-	return this._amount;
-    }
-
-    set amount(amount) {
-    if (typeof amount !== 'number') {
-      throw TypeError('amount must be a Number');
-    }
+export default class Pricing {
+  constructor(amount, currency) {
     this._amount = amount;
-  }
-
-    get currency() {
-	return this._currency;
-    }
-
-    set currency(currency) {
-    if (!(currency instanceof Currency)) {
-      throw TypeError('currency must be a Currency');
-    }
     this._currency = currency;
   }
 
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') throw TypeError('amount must be a number');
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
+    this._currency = newCurrency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
   displayFullPrice() {
-    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
