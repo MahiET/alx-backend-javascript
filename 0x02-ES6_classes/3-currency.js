a50d2306a865fcbc3429b699b823cec90e978459
MahiET <meazalemma2020@gmail.com>
2022-06-17 11:54:51 +0000

    MONAY

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,50 +1,26 @@
-import Currency from './3-currency';
-
-export default class Pricing {
-  constructor(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
 
-  get amount() {
-    return this._amount;
+  set code(newCode) {
+    this._code = newCode;
   }
 
-  set amount(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('amount must be a number');
-    }
-    this._amount = value;
+  set name(newName) {
+    this._name = newName;
   }
 
-  /**
-   * @returns {Currency}
-   */
-  get currency() {
-    return this._currency;
+  get code() {
+    return this._code;
   }
 
-  /**
-   * @param {Currency} value
-   */
-  set currency(value) {
-    if (!(value instanceof Currency)) {
-      throw new TypeError('currency must be a Currency');
-    }
-    this._currency = value;
+  get name() {
+    return this._name;
   }
 
-  displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
-  }
-
-  static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('amount must be a number');
-    }
-    if (typeof conversionRate !== 'number') {
-      throw new TypeError('conversionRate must be a number');
-    }
-    return amount * conversionRate;
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
